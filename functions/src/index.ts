3948dcd080302de1c7299a450a52891595e2b48d
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-07-16 20:40:16 +0700

    up token

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,10 +9,10 @@ exports.sendNotification = functions.https.onRequest(async (req, res) => {
     // Fetch data from the other API
     const response = await axios.get("https://sgp1.blynk.cloud/external/api/get?token=9PWWYxhkSuCnr4OD3VoKrfCPx0WsC4O7&v1");
     const value = response.data;
-    const token = "fmEEUVhXSkm40Cnn2dAdvA:APA91bGdOAlmb979bxqM3JAoh9pgK74-D9tuJoPfkUT7DcxrgXIDzvSjS7NXlma3tMc2k9kVdemgrqI6urqyw8oxAxEvE5j9LkQT7SmHIGPWMIA_7dbU76NlFDyxGQ7ZfCONQyShFso_";
+    const token = "ek717vhASwmNLSiRbRn44V:APA91bEDrWmSTrcvbnW9mZHRjXZchVxYMep0l2v-qyGw_-70N4e6T3-xDPuIoObXqYlSD1aIuMY4H-GbSgWX3MjC3CvMs93HSQnbqPmCnqkBwNq9p3K4S7EZ0iDuof-NEpqpPYu9Hvzf";
 
     // Evaluate condition based on data from the other API
-    if (value >= 100) {
+    if (value >= 400) {
       const message: admin.messaging.Message = {
         notification: {
           title: "Periksa Gas Anda",
