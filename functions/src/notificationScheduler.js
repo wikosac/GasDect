1bb3e96baf039400a7417be7f8189a5843aeb94e
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-07-16 15:04:24 +0700

    using js instead ts

--- functions/src/notificationScheduler.js
+++ functions/src/notificationScheduler.js
@@ -2,13 +2,13 @@ const axios = require("axios");
 
 async function sendNotification() {
   try {
-    await axios.get("http://127.0.0.1:5001/gas-monitor-6692b/us-central1/sendNotification");
+    const data = await axios.get("https://us-central1-gas-monitor-6692b.cloudfunctions.net/sendNotification");
     console.log("Notification sent successfully");
-  } catch (error: any) {
+  } catch (error) {
     console.error("Error sending notification:", error.message);
   }
 }
 
 // Call the function at the desired interval
 sendNotification();
-setInterval(sendNotification, 30000); // Every 60 seconds
+setInterval(sendNotification, 10000); // Every 60 seconds
