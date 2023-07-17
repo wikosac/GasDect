162bd75bf578d75ec4a89afe7eb48a7645a0fb8a
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-07-17 14:11:35 +0700

    js

--- functions/run.js
+++ functions/run.js
@@ -19,7 +19,7 @@ async function sendNotification() {
     await axios.get("https://us-central1-gas-monitor-6692b.cloudfunctions.net/sendNotification");
     //local test
     // await axios.get("http://127.0.0.1:5001/gas-monitor-6692b/us-central1/sendNotification");
-  } catch (error: any) {
+  } catch (error) {
     console.error("Error sending notification:", error.message);
   }
 }
