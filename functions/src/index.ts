1b25654b6971e4fa359dce510c2aac5ef7cd0718
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-07-17 13:43:16 +0700

    save

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,9 +9,9 @@ exports.sendNotification = functions.https.onRequest(async (req, res) => {
     // Fetch data from the other API
     const response = await axios.get("https://sgp1.blynk.cloud/external/api/get?token=9PWWYxhkSuCnr4OD3VoKrfCPx0WsC4O7&v1");
     //token imam
-    // const token = "ek717vhASwmNLSiRbRn44V:APA91bEDrWmSTrcvbnW9mZHRjXZchVxYMep0l2v-qyGw_-70N4e6T3-xDPuIoObXqYlSD1aIuMY4H-GbSgWX3MjC3CvMs93HSQnbqPmCnqkBwNq9p3K4S7EZ0iDuof-NEpqpPYu9Hvzf";
+    const token = "ek717vhASwmNLSiRbRn44V:APA91bEDrWmSTrcvbnW9mZHRjXZchVxYMep0l2v-qyGw_-70N4e6T3-xDPuIoObXqYlSD1aIuMY4H-GbSgWX3MjC3CvMs93HSQnbqPmCnqkBwNq9p3K4S7EZ0iDuof-NEpqpPYu9Hvzf";
     //token iwso
-    const token = "fmEEUVhXSkm40Cnn2dAdvA:APA91bGdOAlmb979bxqM3JAoh9pgK74-D9tuJoPfkUT7DcxrgXIDzvSjS7NXlma3tMc2k9kVdemgrqI6urqyw8oxAxEvE5j9LkQT7SmHIGPWMIA_7dbU76NlFDyxGQ7ZfCONQyShFso_";
+    // const token = "fmEEUVhXSkm40Cnn2dAdvA:APA91bGdOAlmb979bxqM3JAoh9pgK74-D9tuJoPfkUT7DcxrgXIDzvSjS7NXlma3tMc2k9kVdemgrqI6urqyw8oxAxEvE5j9LkQT7SmHIGPWMIA_7dbU76NlFDyxGQ7ZfCONQyShFso_";
 
     const message: admin.messaging.Message = {
       notification: {
