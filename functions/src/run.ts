4740c408e9f65839d11ac262fe37814c631d05f2
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-07-17 12:09:46 +0700

    move logic

---
+++ functions/src/run.ts
@@ -0,0 +1,23 @@
+const axios = require("axios");
+
+setInterval(run, 5000); // Every 5 seconds
+
+// Call the function at the desired interval
+function run() {
+  const response = axios.get("https://sgp1.blynk.cloud/external/api/get?token=9PWWYxhkSuCnr4OD3VoKrfCPx0WsC4O7&v1");
+  if (response.data >= 400) {
+    sendNotification();
+    console.log("Notification sent successfully");
+  } else {
+    console.log("Condition not met, skipping notification");
+  }
+}
+
+async function sendNotification() {
+  try {
+    // await axios.get("https://us-central1-gas-monitor-6692b.cloudfunctions.net/sendNotification");
+    await axios.get("http://127.0.0.1:5001/gas-monitor-6692b/us-central1/sendNotification");
+  } catch (error: any) {
+    console.error("Error sending notification:", error.message);
+  }
+}
